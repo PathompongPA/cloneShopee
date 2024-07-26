63829a34f2194f9aa2a9a7d4dd5d1e5a712f0f8b
Pathompong <pathompong.litk@gmail.com>
2024-07-26 19:47:04 +0700

    refactor(ProductList): change style id to className and clean up return ProductList

--- src/Components/ProductsList/ProductList.jsx
+++ src/Components/ProductsList/ProductList.jsx
@@ -1,51 +1,39 @@
 import "./ProductList.css";
 import { useContext, useEffect } from "react";
 import { SomeDate } from "../../App";
-import { Link } from "react-router-dom";
 import CardProduct from "./cardProduct/cardProduct";
 import GetApi from "../../Initials/GetApi";
+import BtnShowMoreProduct from "./BtnShowMoreProduct/BtnShowMoreProduct";
 
 export default function ProductListComponent() {
-  const { state, dispatch } = useContext(SomeDate);
-  // let a = 0;
-  useEffect(() => {
-    console.log("useEffect Product component");
-    const url = `https://dummyjson.com/products/?limit=${state.numItem}`;
+  const { globalState, dispatch } = useContext(SomeDate);
+  let state = globalState.ProductDummy
 
+  useEffect(() => {
+    let url = `https://dummyjson.com/products/?limit=${globalState.numItem}`;
     GetApi(url, "GET").then(async (result) => {
       await dispatch({ type: "setDateProduct", payload: result });
     });
-  }, [dispatch, state.numItem, state.scrollPosition]);
+  }, [dispatch, globalState.numItem, globalState.scrollPosition, globalState.ProductDummy]);
 
-  return (
-    <div id="boxProductComponent">
-      <div id="boxContentProductComponent">
-        {state.ProductDummy &&
-          state.ProductDummy.products.map((props) => {
-            return (
-              <CardProduct
-                value={props}
-                discount={true}
-                isFavorite={true}
-                key={props.id}
-              />
-            );
-          })}
-      </div>
-      {state.ProductDummy &&
-      state.ProductDummy.limit !== state.ProductDummy.total ? (
-        <div
-          id="showMoreProduct"
-          onClick={() => {
-            console.log("click ShowMore");
-            dispatch({ type: "setNumItem" });
-          }}
-        >
-          <Link id="showMore">show more</Link>
+
+  if (state !== undefined) {
+    let products = state.products
+    const ListCardProducts = products.map(props =>
+      <CardProduct
+        value={props}
+        discount={true}
+        isFavorite={true}
+        key={props.id}
+      />
+    )
+
+    return (
+      <div className="ProductList">
+        <div className="ProductList__BoxListCardProduct">
+          {ListCardProducts}
         </div>
-      ) : (
-        <></>
-      )}
-    </div>
-  );
+        <BtnShowMoreProduct />
+      </div>)
+  }
 }
