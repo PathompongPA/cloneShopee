a524d5715cc90b335315cb4ab69886ddf9d36a25
Pathompong <pathompong.litk@gmail.com>
2024-07-26 19:32:31 +0700

    refactor(HomeTemplate): change style from id to className and clean up

--- src/Templates/HomeTemplate/HomeTemplate.jsx
+++ src/Templates/HomeTemplate/HomeTemplate.jsx
@@ -1,19 +1,23 @@
 import { useEffect } from "react";
 import { FooterComponent, NavbarComponent } from "../../Components";
-import "./HomeTemplate.css";
 import { Outlet } from "react-router-dom";
-import { GoToTop } from "../../Initials";
+import "./HomeTemplate.css";
+
 export default function HomeTemplate() {
+
   useEffect(() => {
     GoToTop();
   }, []);
+
   return (
-    <div id="boxHomeTemplate">
+    <div className="HomeTemplate">
       <NavbarComponent />
-      <div id="boxMainTemplateContent">
-        <Outlet />
-      </div>
+      <Outlet />
       <FooterComponent />
     </div>
   );
+}
+
+const GoToTop = () => {
+  window.scrollTo(0, 0);
 }
