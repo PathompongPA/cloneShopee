b0625fb96641058ec62ae82acaae4a498e7748c3
Pathompong <pathompong.litk@gmail.com>
2024-08-08 23:04:53 +0700

    style(productShop) : add responsive

--- src/Components/Product/shop/productShop.jsx
+++ src/Components/Product/shop/productShop.jsx
@@ -1,11 +1,20 @@
 import "./productShop.css";
+import { useContext } from "react";
+import { SomeDate } from "../../../App";
 
 export default function ProductShop() {
+  const { globalState } = useContext(SomeDate)
+  let product = globalState.ShowProduct
+  let brand = product.brand
+  let description = product.warrantyInformation
+
+
   return (
     <div className="product-shop">
-      <div className="product-shop__banner">Brander</div>
-      <div className="product-shop__content">
-        <div className="content__1">some</div>
+      <div className="product-shop__img-shop">img shop</div>
+      <div className="product-shop__title">
+        <div className="title__name">{brand} .shop</div>
+        <div className="title__description">{description}</div>
       </div>
     </div>
   );
