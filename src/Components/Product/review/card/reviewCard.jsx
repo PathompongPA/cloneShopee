4057ae214be9ca8e0bcd17547aedcbf7f8346952
Pathompong <pathompong.litk@gmail.com>
2024-08-13 02:04:39 +0700

    feature(productReview) : add review component

---
+++ src/Components/Product/review/card/reviewCard.jsx
@@ -0,0 +1,40 @@
+import "./reviewCard.css"
+
+export default function ReviewCard(prop) {
+    let review = prop.reviews
+    let name = review.reviewerName
+    let rating = review.rating
+    let comment = review.comment
+    let date = review.date.split("T")[0]
+    let email = review.reviewerEmail
+    let maximumRating = 5
+    let keyForMap = prop.Key
+
+    const indexRating = []
+    for (let index = 1; index < maximumRating + 1; index++) {
+        if (index <= rating) {
+            indexRating.push(true)
+        } else {
+            indexRating.push(false)
+        }
+    }
+    const Star = indexRating.map((prop, index) => {
+        if (prop) {
+            return <div className="material-symbols-outlined" key={index}>star</div>
+        } else {
+            return <div className="material-symbols-outlined material-symbols-outlined--disable" key={index}>star</div>
+        }
+    })
+
+    return (
+        <div className="review-card" key={keyForMap}>
+            <div className="review-card__name" key={keyForMap}>{name}</div>
+            <div className="review-card__email" key={keyForMap}>{"<" + email + ">"}</div>
+            <div className="review-card__ratting" key={keyForMap}>
+                {Star}
+            </div>
+            <div className="review-card__comment" key={keyForMap}>{comment}</div>
+            <div className="review-card__date" key={keyForMap}>{date}</div>
+        </div>
+    )
+}
--- src/Components/Product/review/productReview.jsx
+++ src/Components/Product/review/productReview.jsx
@@ -1,8 +1,24 @@
 import "./productReview.css";
+import { useContext } from "react";
+import { SomeDate } from "../../../App";
+import { v4 as uuIdV4 } from "uuid";
+import ReviewCard from "./card/reviewCard";
+
+// productTemplate >> productReview 
 export default function ProductReview() {
-  return (
-    <div id="boxReviewComponent">
-      <div>ReviewComponent</div>
-    </div>
-  );
+  const { globalState } = useContext(SomeDate);
+
+  let isHaveStateShowProduct = globalState.ShowProduct !== undefined
+  if (isHaveStateShowProduct) {
+    let showProduct = globalState.ShowProduct
+    let reviews = showProduct.reviews
+    return (
+      <div className="product-review">
+        <div className="product-review__title"> Product review</div>
+        {reviews.map(props =>
+          <ReviewCard reviews={props} key={uuIdV4()} />
+        )}
+      </div>
+    )
+  }
 }
