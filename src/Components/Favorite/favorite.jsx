a0de59b9e2d51fa99907f8aa557520636388818a
Pathompong <pathompong.litk@gmail.com>
2024-08-13 15:29:07 +0700

    refactor(favorite) : change style to `BEM` ,new styling

--- src/Components/Favorite/favorite.jsx
+++ src/Components/Favorite/favorite.jsx
@@ -1,45 +1,28 @@
 import "./favorite.css";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { SomeDate } from "../../App";
 import CardProduct from "../ProductsList/cardProduct/cardProduct";
 
 export default function FavoriteComponent() {
   const { globalState } = useContext(SomeDate);
-  const test = []
-  const test2 = [2, 2, 2, 2,]
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  let productsFavorite = globalState.productsFavorite
+  let isProductsFavoriteZero = productsFavorite.length === 0
 
   return (
-    <div id="boxFavoriteComponent">
-      <div id="boxContentFavoriteComponent">
-        <div id="boxTitleFavoriteComponent">
-          <p>favorite</p>
+    <div className="favorite">
+      <div className="favorite__title">Product Favorite</div>
+      {isProductsFavoriteZero ?
+        <div className="favorite__no-product">no item </div> :
+        <div className="favorite__list">
+          {productsFavorite.map(props =>
+            <CardProduct
+              discountIconOn={true}
+              favoriteIconOn={true}
+              productDetail={props}
+              key={props.id} />
+          )}
         </div>
-        <div id="boxCardFavorite">
-          {test.map(ele => console.log(ele))}
-          {test2.map(ele => console.log(ele))}
-          {
-            globalState.productsFavorite !== undefined ? (
-              globalState.productsFavorite.map((props) => {
-                let productDetail = props
-                return (
-                  <CardProduct
-                    discountIconOn={false}
-                    favoriteIconOn={true}
-                    productDetail={productDetail}
-                    key={productDetail.id}
-                  />
-                )
-              })
-            ) : (
-              <div id="noFavoriteItem">No one favorite item.</div>
-            )
-          }
-        </div>
-      </div>
+      }
     </div>
   );
 }
